74b337f111327e87c75fd4e04d596f2f346f1611
Dan Porter <dan.porter@shopify.com>
2022-04-08 15:35:00 -0400

    Remove brew shit.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ GPG_TTY=$(tty)
 # if [ "$SPIN" ]; then
 #     sudo apt-get install -y specific-package
 # fi
+
+# if [ -d '/usr/local/opt/ruby/bin' ];
+#    export PATH="/usr/local/opt/ruby/bin:$PATH"
+# fi
--- zshrc
+++ zshrc
@@ -1,6 +1,1 @@
 export EDITOR=emacs
-
-# configure homebrew
-# https://github.com/Homebrew/brew/blob/master/Library/Homebrew/cmd/shellenv.sh
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
